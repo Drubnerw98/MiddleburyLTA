695075d3b37d4383313dcd597cdaf7327c5d1139
drubnerw98 <drubnerw98@gmail.com>
2025-06-20 19:21:19 -0400

    WEBSITE VISUAL OVERHAUL DRAFT 1

--- src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from "../components/RegisterForm";
-
-export default function RegisterPage() {
-  return (
-    <div className="min-h-[70vh] px-4">
-      <RegisterForm />
-    </div>
-  );
-}
